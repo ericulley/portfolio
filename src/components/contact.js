637ec83bcbd264178e653ade17d87ac5f22ddfb0
Your Name <you@example.com>
2022-10-23 16:51:48 -0600

    updated resume link

--- src/components/contact.js
+++ src/components/contact.js
@@ -14,7 +14,7 @@ class Contact extends React.Component {
                 <h1 className="header">&gt;<span id="contact-header">Contact</span></h1>
                 <div id="link-container">
                     <p className="contact-link" id="email-link"></p>
-                    <a className="contact-link" href="https://docs.google.com/document/d/12DQ_ngWROPPsRIxHTw7v-bN3mI7LAW2zrTYCeYFwtMg/export?format=pdf" rel="noreferrer" >Resume</a>
+                    <a className="contact-link" href="https://docs.google.com/document/d/1j-Q7AOK4YHVJTt3bJTBrc6Mu0tV5rmsdnfGc5JcU1NE/export?format=pdf" rel="noreferrer" >Resume</a>
                     <a className="contact-link" href="https://www.linkedin.com/in/ericculley" rel="noreferrer" target="_blank">LinkedIn</a>
                 </div>
             </div>
