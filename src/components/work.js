dbb07d69481b6e9432e468cc70c0fc3679411a6e
Eric Culley <ericulley@gmail.com>
2023-01-13 19:51:18 -0700

    work typos

--- src/components/work.js
+++ src/components/work.js
@@ -10,19 +10,19 @@ class Work extends React.Component {
                 <div id="project-container">
                     <div className="projects">
                         <h3><a className="work-link" href="https://coin-purse.herokuapp.com/" target="_blank">Coin Purse</a></h3>
-                        <em>Technologies: JS/React, Java/Spring, Chart.js</em>
+                        <em>Technologies: React, Java/Spring, Chart.js</em>
                         <p>A cryptocurrency portfolio app</p>
                         <a className="github-link" href="https://github.com/ericulley/coin_purse" target="_blank">FrontEnd <img className="github-img" src={process.env.PUBLIC_URL + '/GitHub.png'} alt="github-logo"/></a> / <a className="github-link" href="https://github.com/ericulley/project5" target="_blank">BackEnd <img className="github-img" src={process.env.PUBLIC_URL + '/GitHub.png'} alt="github-logo"/></a>
                     </div>
                     <div className="projects">
                         <h3><a className="work-link" href="https://personal-backlog.vercel.app/" target="_blank">Personal Backlog</a></h3>
-                        <em>Technologies: Typescript/Node.js/Express/Vite</em>
+                        <em>Technologies: Typescript, Node.js/Express, Vite</em>
                         <p>A drap-n-drop SPA for your personal backlog of issues.</p>
                         <a href="https://github.com/ericulley/personal-backlog" target="_blank"><img className="github-img" src={process.env.PUBLIC_URL + '/GitHub.png'} alt="github-logo"/></a>
                     </div>
                     <div className="projects">
                         <h3><a className="work-link" href="http://imoutdoorsy-env.eba-s3p8x6p8.us-west-2.elasticbeanstalk.com/" target="_blank">I'm Outdoorsy</a></h3>
-                        <em>Technologies: JS, Node.js/Express, Cloudinary</em>
+                        <em>Technologies: Node.js/Express, Cloudinary API</em>
                         <p>A social media app for the outdoorsy</p>
                         <a href="https://github.com/ericulley/ImOutdoorsy" target="_blank"><img className="github-img" src={process.env.PUBLIC_URL + '/GitHub.png'} alt="github-logo"/></a>
                     </div>
