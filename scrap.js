d294f88c08df5bafd571e5ff3914573586625e1f
Eric Culley <ericulley@gmail.com>
2023-08-28 23:12:08 -0600

    deleted scrap file

--- scrap.js
+++
@@ -1,7 +0,0 @@
-// Cabin Fever Portfolio Piece 
-<div className="projects">
-    <h3><a className="work-link" href="#">Cabin Fever <span id='under-construction'>(Under Construction)</span></a></h3>
-    <em>Technologies: JS/React, Ruby/Rails, MapBox</em>
-    <p>A cabin rental and reservation app</p>
-    <a href="" target="_blank"><img className="github-img" src={process.env.PUBLIC_URL + '/GitHub.png'} alt="github-logo"/></a>
-</div>
