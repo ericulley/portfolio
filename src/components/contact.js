ffa36ba7a36ee4361f14e52f00826cef1f8f7822
Eric Culley <ericulley@gmail.com>
2023-08-31 20:13:04 -0600

    added github link

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,25 +1,36 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 class Contact extends React.Component {
-    componentDidMount = () => {
-        document.querySelector('#email-link').addEventListener('click', () => {
-            navigator.clipboard.writeText('ericulley@gmail.com')
-        })
-    }
-    render = () => {
-        return (
-            <div className="container" id="contact-container">
-                <Link to='/'><h2 className="back">Eric</h2></Link>
-                <h1 className="header">&gt;<span id="contact-header">Contact</span></h1>
-                <div id="link-container">
-                    <p className="contact-link" id="email-link"></p>
-                    <a className="contact-link" href="https://docs.google.com/document/d/1mr-ZtDFYrQVUzLk5naKndox8y2A6CViQQ19Lh1KjXew/export?format=pdf" rel="noreferrer" >Resume</a>
-                    <a className="contact-link" href="https://www.linkedin.com/in/ericculley" rel="noreferrer" target="_blank">LinkedIn</a>
-                </div>
-            </div>
-        )
-    }
+  componentDidMount = () => {
+    document.querySelector("#email-link").addEventListener("click", () => {
+      navigator.clipboard.writeText("ericulley@gmail.com");
+    });
+  };
+  render = () => {
+    return (
+      <div className="container" id="contact-container">
+        <Link to="/">
+          <h2 className="back">Eric</h2>
+        </Link>
+        <h1 className="header">
+          &gt;<span id="contact-header">Contact</span>
+        </h1>
+        <div id="link-container">
+          <p className="contact-link" id="email-link"></p>
+          <a className="contact-link" href="https://docs.google.com/document/d/1mr-ZtDFYrQVUzLk5naKndox8y2A6CViQQ19Lh1KjXew/export?format=pdf" rel="noreferrer">
+            Resume
+          </a>
+          <a className="contact-link" href="https://www.linkedin.com/in/ericculley" rel="noreferrer" target="_blank">
+            LinkedIn
+          </a>
+          <a className="contact-link" href="https://github.com/ericulley" rel="noreferrer" target="_blank">
+            GitHub
+          </a>
+        </div>
+      </div>
+    );
+  };
 }
 
-export default Contact
+export default Contact;
